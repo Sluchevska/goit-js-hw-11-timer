c764b70f53589ef859d2798cffcfd83fa11eb96b
Sluchevska <alaska3a@gmail.com>
2021-07-07 22:07:42 +0100

    update

--- src/timer.js
+++ src/timer.js
@@ -46,7 +46,7 @@ class CountdownTimer {
 };
 const timer =  new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2021'),    
+    targetDate: new Date('Jul 30, 2021'),    
 });
 
 
