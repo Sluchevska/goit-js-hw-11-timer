b9c7332e2ed1c669986ac6d8526d7ab9032cddd4
Sluchevska <alaska3a@gmail.com>
2021-07-07 21:51:00 +0100

    update

--- src/timer.js
+++ src/timer.js
@@ -1,74 +1,102 @@
 
 import './sass/main.scss';
 
-const bodyRef = document.querySelector('body')
-bodyRef.insertAdjacentHTML("afterbegin", `<div class="timer" id="timer-1"></div>`)
-const targetTime = new Date(2021, 6, 25, 11, 11, 11).getTime()
-console.log(targetTime)
+// const bodyRef = document.querySelector('body')
+// bodyRef.insertAdjacentHTML("afterbegin", `<div class="timer" id="timer-1"></div>`)
+// const targetTime = new Date(2021, 6, 25, 11, 11, 11).getTime()
+// console.log(targetTime)
 
-timer()
+class CountdownTimer{
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetTime;
+        this.refs = {
+            days: document.querySelector('[data-value="days"]'),
+            hours: document.querySelector('[data-value= "hours"]'),
+            mins: document.querySelector('[data-value="mins"]'),
+            secs: document.querySelector('[data-value="secs"]'),
+        };
+        setInterval(() => {
+    const startDate = Date.now();
+    const time = this.targetDate - startDate;
+    const { days, hours, mins, secs } = this.getTimeComponents(time);
+    this.updateClockFace(this.getTimeComponents(time));
 
-function timer() {
-    let intID = setInterval(function()
-    {
-        const currentTime = new Date().getTime();
-    
-        const time = targetTime - currentTime;
-        const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-                // document.getElementById('timer-1').innerHTML = days + "d " + hours + "h " + mins + "m " + secs + "s ";
-            const timerBox = document.getElementById('timer-1')
-            timerBox.innerHTML = `<div class="field">
-            <span class="value" data-value="days">${days}</span>
-            <span class="label">Days</span></div>
-            <div class="field">
-            <span class="value" data-value="hours">${hours}</span>
-            <span class="label">Hours</span></div>
-    
-        <div class="field">
-            <span class="value" data-value="mins">${mins}</span>
-            <span class="label">Minutes</span>
-        </div>
-    
-        <div class="field">
-            <span class="value" data-value="secs">${secs}</span>
-            <span class="label">Seconds</span>
-        </div>`  
-            if (time < 0) {
-                        clearInterval(intID);
-                      document.getElementById('timer-1').innerHTML = "Its time NOW" 
-                }
-    }, 1000)
-
-}
-
-function pad(value) {
-        return String(value).padStart(2, '0');
+}, 1000)
+    }
+    getTimeComponents(time) {
+       const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000)); 
+    }
 }
 
 
 
+// timer()
 
-// function getTimeComponents(time) {
-// 
-//     console.log(days)
-// console.log(hours)
-// console.log(mins)
-// console.log(secs)
-//     return {
-//         days, hours, mins, secs
-//     }
+// function timer() {
+//     let intID = setInterval(function()
+//     {
+//         const currentTime = new Date().getTime();
     
+//         const time = targetTime - currentTime;
+//         const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+// const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+// const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//         const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+//                 // document.getElementById('timer-1').innerHTML = days + "d " + hours + "h " + mins + "m " + secs + "s ";
+//             const timerBox = document.getElementById('timer-1')
+//             timerBox.innerHTML = `<div class="field">
+//             <span class="value" data-value="days">${days}</span>
+//             <span class="label">Days</span></div>
+//             <div class="field">
+//             <span class="value" data-value="hours">${hours}</span>
+//             <span class="label">Hours</span></div>
+    
+//         <div class="field">
+//             <span class="value" data-value="mins">${mins}</span>
+//             <span class="label">Minutes</span>
+//         </div>
+    
+//         <div class="field">
+//             <span class="value" data-value="secs">${secs}</span>
+//             <span class="label">Seconds</span>
+//         </div>`  
+//             if (time < 0) {
+//                         clearInterval(intID);
+//                       document.getElementById('timer-1').innerHTML = "Its time NOW" 
+//                 }
+//     }, 1000)
+
+// }
+
+// function pad(value) {
+//         return String(value).padStart(2, '0');
 // }
 
 
 
-// new CountdownTimer({
-//  selector: '#timer-1',
-// targetDate: new Date('Jul 17, 2019'),
-// });
+
+// // function getTimeComponents(time) {
+// // 
+// //     console.log(days)
+// // console.log(hours)
+// // console.log(mins)
+// // console.log(secs)
+// //     return {
+// //         days, hours, mins, secs
+// //     }
+    
+// // }
+
+
+
+// // new CountdownTimer({
+// //  selector: '#timer-1',
+// // targetDate: new Date('Jul 17, 2019'),
+// // });
 
 
 
